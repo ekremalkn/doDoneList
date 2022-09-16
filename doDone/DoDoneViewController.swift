683277edd1c3c84ff1bf21216da8a9f059a93e2c
Ekrem Alkan <ae.alkanekrem@gmail.com>
2022-09-16 14:26:27 +0300

    added add add button,checkmark, alert, UITextField

--- doDone/DoDoneViewController.swift
+++ doDone/DoDoneViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class DoDoneViewController: UITableViewController {
 
-    let itemArray = ["ekrem", "fatih"]
+    var toDoArray = ["ekrem", "fatih"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,27 +17,80 @@ class DoDoneViewController: UITableViewController {
 
     
     //MARK: - verileri satırlara ekleme
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return toDoArray.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DoDoneItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        
+          
         
         return cell
         
     }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+      
+        }
+    
+    
+    //MARK: - Add Button
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var alertTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Unutmamak için listeye ekle!", message: "", preferredStyle: .alert)
+       
+        alert.addTextField { (textField) in
+            textField.placeholder = "örn: Alışveriş"
+            alertTextField = textField
+            
+            
+           }
+        
+        
+        alert.addAction(UIAlertAction(title: "Ekle", style: .default, handler: {
+            (action: UIAlertAction!) in
+            
+            self.toDoArray.append(alertTextField.text!)
+            self.tableView.reloadData()
+            
+            
+        }))
+        
+        
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+  
+    
+    
+       
+                
+    }
+    
+    
 
 
-}
+
+
 
