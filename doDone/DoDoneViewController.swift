e678021fb82509f4d0b00373ad9def2085e1fd36
Ekrem Alkan <ae.alkanekrem@gmail.com>
2022-09-16 12:36:27 +0300

    verileri satırlara ekledim

--- doDone/DoDoneViewController.swift
+++ doDone/DoDoneViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class DoDoneViewController: UITableViewController {
 
+    let itemArray = ["ekrem", "fatih"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    
+    //MARK: - verileri satırlara ekleme
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DoDoneItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
     }
 
 
